c477ea64f4ed9d13e1510ff094e04e26c357ef9a
Aston Rasen <Ventillate@users.noreply.github.com>
2015-08-05 23:46:03 -0400

    Implemented New Command ,favfood
    The ,favfood command will now allow users holding ranks Voice and up to list their own favorite food on the bot.

--- commands/cafe customs.js
+++ commands/cafe customs.js
@@ -1,4 +1,4 @@
-Settings.addPermissions(['plug', 'info', 'notarealcafe', 'website', 'doeszalmmakegoodrobots', menu]);
+Settings.addPermissions(['plug', 'info', 'notarealcafe', 'website', 'doeszalmmakegoodrobots', 'menu', 'favfood']);
 
 exports.commands = {
 	plug: function (arg, by, room, cmd) {
@@ -40,12 +40,62 @@ exports.commands = {
 	guide: 'info',
 	botguide: 'info',
 	info: function (arg, by, room, cmd) {
-		var text = "For help with this bot, please visit http://pastebin.com/raw.php?i=A0yAxPgv";
+		var text = "For help with this bot, please visit: http://pastebin.com/raw.php?i=A0yAxPgv";
 		if (!this.isRanked('+')) {
 			this.pmReply(text);
 		} else {
 			this.reply(text);
 		}
+	},
+	favfood: function(arg, by, room) { 
+        	var usr = toId(arg);
+        	var inValue = arg.split(' ');
+        	var found = false;
+        	if (!arg) return this.say(room, "Please include a name.");
+        	if (!this.settings.favfood) {
+        	    this.settings.favfood = [];
+        	    this.writeSettings();
+        	}
+        	var favfoods = this.settings.favfoods;
+        	var output = [];
+        	for (i = 1; i < inValue.length; i++) {
+        	    output.push(inValue[i]);
+        	}
+        	var output = output.join(" ");
+        	if (toId(inValue[0]) === "set" && inValue.length > 1) {
+        	    if (!this.settings.favfoods) {
+        	        this.settings.favfoods = [];
+        	        this.writeSetting();
+        	        return this.say(room, "Sorry, please try that again. Had to set a few things up.");
+        	    }
+        	    if (this.hasRank(by, '+%@#&~')) {
+        	        for (i = 0; i < favfoods.length; i++) {
+        	            if (this.settings.favfoods[i].name === toId(by)) {
+        	                this.settings.favfoods[i].favfood = output;
+        	                this.say(room, "Favorite food for user " + by + " has been edited.");
+        	                return this.writeSettings();
+        	            }
+        	        }
+        	        var favoritefood = {
+        	            "name":toId(by),
+        		            "favfood":output
+        	        };
+        	        this.settings.favfoods.push(favoritefood);
+        	        this.writeSettings();
+        	        return this.say(room, "Favorite food for user " + by + " has been set.");
+        	    } else {
+        	        return this.say(room, "Sorry, but you need to be at least a voice to set your favorite food.");
+        	    }
+        	} //Code for setting users' favorite food.
+        	for (i = 0; i < favfoods.length; i++) {
+        		if (usr === favfoods[i].name) {
+        	        found = true;
+        	        return this.say(room, toTitleCase(usr) + ": " + favfoods[i].favfood);
+        	    }
+        	}
+        	if (found === false) {
+        	    this.say(room,"This user does not have a favorite food.");
+        	}
 	},
 
 	nocafe: 'notarealcafe',
